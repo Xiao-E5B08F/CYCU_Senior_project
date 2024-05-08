92a938f997eb16f60a7b43f581f37e5b4c1e27ec
YCLiu <liuyancheng02@gmail.com>
2024-05-09 04:51:02 +0800

    chore: Update pose_landmarker model asset path and video filename

--- main.py
+++ main.py
@@ -8,9 +8,8 @@ from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 
 
-def draw_landmarks_on_image(rgb_image, result):
-    pose_landmarks_list = result.pose_landmarks
-    # pose_landmarks_list = detection_result
+def draw_landmarks_on_image(rgb_image, detection_result):
+    pose_landmarks_list = detection_result.pose_landmarks
     annotate_image = np.copy(rgb_image)
 
     # Loop through the detected poses to visualize.
@@ -33,8 +32,8 @@ def draw_landmarks_on_image(rgb_image, result):
 def create_pose_landmarker():
     # Create an PoseLandmarker object.
     # setting what kind of model we use
-    base_options = mp.tasks.BaseOptions(model_asset_path='pose_landmarker.task')
-    # ,delegate=python.BaseOptions.Delegate.GPU)
+    base_options = mp.tasks.BaseOptions(model_asset_path='pose_landmarker_full.task')
+    #                                    , delegate=mp.tasks.BaseOptions.Delegate.GPU)
     options = vision.PoseLandmarkerOptions(
         base_options=base_options,
         running_mode=vision.RunningMode.VIDEO,
@@ -42,29 +41,39 @@ def create_pose_landmarker():
 
     with vision.PoseLandmarker.create_from_options(options) as landmarker:
         # Load the input video and setting output video.
-        video = cv2.VideoCapture("Fencing.mp4")
-        fps = video.get(cv2.CAP_PROP_FPS)
+        filename = "Fencing1080p.mp4"
+        video = cv2.VideoCapture(filename)
+        if not video.isOpened():
+            print("Cannot open camera")
+            exit()
         width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = video.get(cv2.CAP_PROP_FPS)
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        output = cv2.VideoWriter("Fencing_drew.mp4", fourcc, fps, (width, height))
+        output = cv2.VideoWriter("Drew_" + filename, fourcc, fps, (width, height))
 
-        percent_target = 1
+        percent_target = 0
         # Loop every frame and annotated the frame
         total_frame = video.get(cv2.CAP_PROP_FRAME_COUNT)
         for i in range(int(total_frame)):
             success, frame = video.read()
-            frame = cv2.resize(frame, (520, 300))  # resize
+            # frame = cv2.resize(frame, (width, height))  # resize
             frame_timestamp_ms = int(video.get(cv2.CAP_PROP_POS_MSEC))
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-            detection_result = landmarker.detect_for_video(mp_image, frame_timestamp_ms)
-            annotated_image = draw_landmarks_on_image(frame, detection_result)
+            detection_result = landmarker.detect_for_video(mp_image, frame_timestamp_ms)  # detect the pose
+            annotated_image = draw_landmarks_on_image(frame, detection_result)  # draw the landmarks on the frame
             output.write(annotated_image)
-            # cv2.imshow('frame', annotated_image)
-
+            '''
+            cv2.imshow("annotated_image", annotated_image)
+            if cv2.waitKey(1) == ord('q'):
+                break
+            '''
             if int(100 * i / total_frame) == percent_target:
                 print("{:.2f}%".format(int(100 * i / total_frame)))
-                percent_target += 1
+                percent_target += 10
+        video.release()
+        output.release()
+        cv2.destroyAllWindows()
 
 
 def main():
